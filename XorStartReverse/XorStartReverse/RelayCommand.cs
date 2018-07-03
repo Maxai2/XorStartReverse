9a841b53d73642d441a16884a1f5faad1e4a36c6
Хамелеон <maxali946@gmail.com>
2018-07-03 16:56:33 +0400

    50%

---
+++ XorStartReverse/XorStartReverse/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace XorStartReverse
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> excecute;
+        private Predicate<object> canExcecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
